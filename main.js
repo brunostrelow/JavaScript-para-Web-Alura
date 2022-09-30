6fdf649e7ef7b9ed6adf3cba9b82293c8187b6aa
Bruno Strelow <bruno.strelow@hotmail.com>
2022-09-30 11:02:59 -0300

    Adiciona funcionamento das teclas

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('Olá Mundo!')
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function (){
+        tocaSom(idAudio)
+    }
+}
